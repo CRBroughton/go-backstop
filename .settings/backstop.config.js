7d196cbc6432c38571ec9c0fa6b47739a7eebf74
CRBroughton <crbroughton@posteo.uk>
2023-06-25 13:46:21 +0100

    feat: :sparkles: write function to getScenarios

--- .settings/backstop.config.js
+++ .settings/backstop.config.js
@@ -1,14 +1,19 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
+const fs = require('fs');
 
 const settingsPath = '.settings/config.json'
 
 function getViewports() {
   const data = fs.readFileSync(settingsPath)
   const json = JSON.parse(data)
-  console.log(json.viewports)
   return json.viewports
+}
+
+function getScenarios() {
+    const data = fs.readFileSync(settingsPath)
+    const json = JSON.parse(data)
+    return json.scenarios
 }
 
 module.exports = {
@@ -16,27 +21,7 @@ module.exports = {
     "viewports": getViewports(),
     "onBeforeScript": "puppet/onBefore.js",
     "onReadyScript": "puppet/onReady.js",
-    "scenarios": [
-        {
-            "label": "BackstopJS Homepage",
-            "cookiePath": "backstop_data/engine_scripts/cookies.json",
-            "url": "https://garris.github.io/BackstopJS/",
-            "referenceUrl": "",
-            "readyEvent": "",
-            "readySelector": "",
-            "delay": 0,
-            "hideSelectors": [],
-            "removeSelectors": [],
-            "hoverSelector": "",
-            "clickSelector": "",
-            "postInteractionWait": 0,
-            "selectors": [],
-            "selectorExpansion": true,
-            "expect": 0,
-            "misMatchThreshold": 0.1,
-            "requireSameDimensions": true
-        }
-    ],
+    "scenarios": getScenarios(),
     "paths": {
         "bitmaps_reference": "backstop_data/bitmaps_reference",
         "bitmaps_test": "backstop_data/bitmaps_test",
